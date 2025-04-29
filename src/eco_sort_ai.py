073de5ad10b7d1405850f7a640bfbe34cda35167
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-04-29 17:03:26 +0700

    fix: :bug: cv2 cannot access defined index

--- src/eco_sort_ai.py
+++ src/eco_sort_ai.py
@@ -3,14 +3,18 @@ import cv2
 
 
 class EcoSortAI:
-    def __init__(self, camera_source_index: CameraIndex | int = 0):
+    def __init__(self, camera_source_index: CameraIndex | int = CameraIndex.BUILT_IN):
         """
         Initialize the EcoSortCamera with a camera source index.
 
         :param camera_source_index: The index of the camera source.
         """
-        self.camera_source_index = camera_source_index
-        self.capture = cv2.VideoCapture(camera_source_index)
+        self.camera_source_index = (
+            camera_source_index.value
+            if isinstance(camera_source_index, CameraIndex)
+            else camera_source_index
+        )
+        self.capture = cv2.VideoCapture(self.camera_source_index)
         self.is_opened = self.capture.isOpened()
 
     def start_capture(self):
