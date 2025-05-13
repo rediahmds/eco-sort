3f0c9e08354ed428714fcbdcb2b314acead641ba
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-05-13 17:24:34 +0700

    feat: :sparkles: guard script execution

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from eco_sort_ai import EcoSortAI
+from static.camera_index import CameraIndex
 
-# from static.camera_index import
+
 from pathlib import Path
 
-model_path = Path("models/yolov5n.pt").absolute()
 
-eco_ai = EcoSortAI(camera_source_index=0, model_path=model_path)
-eco_ai.start_capture()
+if __name__ == "__main__":
+    model_path = Path("models/yolov5nu.pt").absolute()
+
+    eco_ai = EcoSortAI(camera_source_index=CameraIndex.BUILT_IN, model_path=model_path)
+    eco_ai.start_capture()
