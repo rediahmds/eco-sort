e6bd6f9e936b1e39b8ce66341ef18e921ef965d7
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-05-19 15:07:59 +0700

    feat: :sparkles: inference from custom model

--- src/eco_sort/eco_sort_ai.py
+++ src/eco_sort/eco_sort_ai.py
@@ -1,4 +1,5 @@
 import math
+import traceback
 from static.camera_index import CameraIndex
 import cv2
 from pathlib import Path
@@ -23,88 +24,6 @@ class EcoSortAI:
             else camera_source_index
         )
 
-        self.class_names = [
-            "person",
-            "bicycle",
-            "car",
-            "motorbike",
-            "aeroplane",
-            "bus",
-            "train",
-            "truck",
-            "boat",
-            "traffic light",
-            "fire hydrant",
-            "stop sign",
-            "parking meter",
-            "bench",
-            "bird",
-            "cat",
-            "dog",
-            "horse",
-            "sheep",
-            "cow",
-            "elephant",
-            "bear",
-            "zebra",
-            "giraffe",
-            "backpack",
-            "umbrella",
-            "handbag",
-            "tie",
-            "suitcase",
-            "frisbee",
-            "skis",
-            "snowboard",
-            "sports ball",
-            "kite",
-            "baseball bat",
-            "baseball glove",
-            "skateboard",
-            "surfboard",
-            "tennis racket",
-            "bottle",
-            "wine glass",
-            "cup",
-            "fork",
-            "knife",
-            "spoon",
-            "bowl",
-            "banana",
-            "apple",
-            "sandwich",
-            "orange",
-            "broccoli",
-            "carrot",
-            "hot dog",
-            "pizza",
-            "donut",
-            "cake",
-            "chair",
-            "sofa",
-            "pottedplant",
-            "bed",
-            "diningtable",
-            "toilet",
-            "tvmonitor",
-            "laptop",
-            "mouse",
-            "remote",
-            "keyboard",
-            "cell phone",
-            "microwave",
-            "oven",
-            "toaster",
-            "sink",
-            "refrigerator",
-            "book",
-            "clock",
-            "vase",
-            "scissors",
-            "teddy bear",
-            "hair drier",
-            "toothbrush",
-        ]
         self.model_path = model_path
 
     def start_capture(self):
@@ -139,12 +58,12 @@ class EcoSortAI:
 
                         conf = math.ceil((box.conf[0] * 100)) / 100
 
-                        cls = box.cls[0]
-                        name = self.class_names[int(cls)]
+                        id = int(box.cls[0])
+                        name = model.names[id]
 
                         cvzone.putTextRect(
                             img=frame,
-                            text=f"{name} " f"{conf}",
+                            text=f"{name} {conf}",
                             pos=(max(0, x1), max(35, y1)),
                             thickness=1,
                             scale=1,
