b91eadc9bf53ba2dd326824e0dd0ff26c3103cf0
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-05-26 13:16:56 +0700

    feat: :sparkles: add more blynk pins]

--- src/static/blynk/blynk_pins.py
+++ src/static/blynk/blynk_pins.py
@@ -10,7 +10,16 @@ class BlynkPins(Enum):
     `V0`: prediction result
 
     `V1`: servo controller
+
+    `V2`: organic bin level indicator
+
+    `V3`: recycle/Non-organic bin level indicator
+
+    `V4`: methane sensor value
     """
 
     V0 = "v0"
     V1 = "v1"
+    V2 = "v2"
+    V3 = "v3"
+    V4 = "v4"
